211a01a14d782f8d7caf8dff4e9cceacfb9d0221
André Boddenberg <andre@boddenberg.it>
2017-07-13 19:50:34 +0200

    Use bootstrap in index.php

--- webroot/index.php
+++ webroot/index.php
@@ -1,27 +1,74 @@
-<html>
-    <head>
-	<title>anbada</title>
-	<link rel="stylesheet" href="anbada.css">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="anbada.ico">
 
-	<meta http-equiv="refresh" content="10">
-	<?php
-		$client_ip = $_SERVER['REMOTE_ADDR'];
-		shell_exec("/var/www/html/anbada.sh $client_ip refresh")
-	?>
-    </head>
+    <title>Anbada</title>
 
-    <body>
-	<?php include("header.html"); ?>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-	<form action="anbada.php" method="post">
-		<p>connected device(s):<p>
-		<?php include("/tmp/anbada/devices"); ?>
-		<br>
-		<label><input type="checkbox" name="apps"</input>apps?</label><br>
-		<label><input type="checkbox" name="storage"</input>storage?</label><br>
-		<label><input type="checkbox" name="system_apps"</input>system apps?</label><br>
-		<br>
-		<input type="submit" class="button" name="action" value="backup"></input>
-	</form>
-    <body>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/starter-template.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand active" href="index.php">Anbada</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="backup.php">backup</a></li>
+            <li><a href="restore.php">restore</a></li>
+            <li><a href="sync.php">sync</a></li>
+            <li><a href="settings.php">settings</a></li>
+            <li><a href="help.php">help</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <form action="anbada.php" method="post">
+      		<p>connected device(s):<p>
+      		<?php include("/tmp/anbada/devices"); ?>
+      		<br>
+      		<label><input type="checkbox" name="apps"</input>apps?</label><br>
+      		<label><input type="checkbox" name="storage"</input>storage?</label><br>
+      		<label><input type="checkbox" name="system_apps"</input>system apps?</label><br>
+      		<br>
+      		<input type="submit" class="button" name="action" value="backup"></input>
+      	</form>
+      </div>
+
+    </div><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
 </html>
