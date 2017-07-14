4602d113bf4cc75d29f1354c551bc146db8c8301
André Boddenberg <andre@boddenberg.it>
2017-07-14 19:32:40 +0200

    Use wlan.php for settings.php

--- webroot/settings.php
+++ webroot/settings.php
@@ -56,7 +56,20 @@
 
           <div class="starter-template">
 
-	<p>awesome settings</p>
+	<p>connected ssid: <?php include("/tmp/andaba/wlan") ?></p>
+	<br>
+	<form action="anbada.php" method="post">
+		<p>add new ssid:</p>
+		<label>ssid:
+			<input type="text" name="ssid"/>
+		</label>
+		<br><br>
+		<label>password:
+			 <input type="password" name="password"/>
+		</label>
+		<br><br>
+		<input type="submit" class="button" name="action" value="add_ssid"></input>
+	</form>
 
 
   </div>
